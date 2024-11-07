963f97fad941dfb68f1ea33a5e7a1c8c34231c7c
Zhemo17 <zhemo17@gmail.com>
2024-11-06 23:05:10 -0600

    - Character Blueprint Setup

--- Source/Aura/Private/Character/RPGCharacterBase.cpp
+++ Source/Aura/Private/Character/RPGCharacterBase.cpp
@@ -7,6 +7,10 @@
 ARPGCharacterBase::ARPGCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick =  false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
