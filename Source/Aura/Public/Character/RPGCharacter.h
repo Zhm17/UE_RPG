343eec5890a65c9e1395339369f91112d2240308
Zhemo17 <zhemo17@gmail.com>
2024-11-06 21:52:44 -0600

    - Player and Enemy Characters

---
+++ Source/Aura/Public/Character/RPGCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/RPGCharacterBase.h"
+#include "RPGCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ARPGCharacter : public ARPGCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Aura/Public/Character/RPGEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/RPGCharacter.h"
+#include "RPGEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ARPGEnemy : public ARPGCharacter
+{
+	GENERATED_BODY()
+	
+};
