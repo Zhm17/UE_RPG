a5594906832c0e16604cc0d78c6ae2aeb1950205
Zhemo17 <zhemo17@gmail.com>
2024-11-19 16:18:09 -0600

    - The Player State

---
+++ Source/Aura/Private/Player/RPGPlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/RPGPlayerState.h"
+
+ARPGPlayerState::ARPGPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
